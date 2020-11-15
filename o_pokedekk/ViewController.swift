8852c66182fd1013e1b64bcb064e96e617acbf04
Pasquale <spisto.pasquale1994@gmail.com>
2020-11-15 18:00:39 +0100

    Initial Commit

---
+++ o_pokedekk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  o_pokedekk
+//
+//  Created by Pasquale Spisto on 15/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
